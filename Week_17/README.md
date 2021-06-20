7d550480fa127080694ec6a7eddcdff87325bfba
qiuwf <qiuwf@Ctrip.com>
2021-06-21 00:27:53 +0800

    提交代码

--- Week_17/README.md
+++ Week_17/README.md
@@ -1,1 +1,24 @@
+
+
 学习笔记
+
+#### 1. 初始化与构建 | 初始化工具Yeoman（一）
+* 安装 
+``` npm install -g yo```
+
+* YEOMAN工具
+
+#### 2. 初始化与构建 | 初始化工具Yeoman（二）
+
+
+#### 3. 初始化与构建 | 初始化工具Yeoman（三）
+* prompt 交互式系统
+* install dependence
+* WORKING WITH THE FILE SYSTEM
+* vue-style-loader
+* copy-webpack-plugin 拷贝html源文件到dist目录
+
+
+#### 3. 初始化与构建 | 初始化工具Yeoman（三）
+
+
---
+++ Week_17/generators/app/templates/webpack.config.js
@@ -0,0 +1,34 @@
+
+
+
+const { VueLoaderPlugin } = require('vue-loader')
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry:'./src/main.js',
+    module: {
+      rules: [
+          {
+            test: /\.vue$/,
+            use:'vue-loader'
+          },
+        {
+          test: /\.css$/,
+          use: [
+              'vue-style-loader',
+              'css-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+        // 请确保引入这个插件！
+        new VueLoaderPlugin(),
+        new CopyPlugin({
+            patterns: [
+              { from: "src/*.html", to: "[name].[ext]" },
+            ],
+          }),
+      ]
+  };
+  
